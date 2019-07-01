251714593ce4c4e48cb11141cb06adfa23a60480
Vlad1235 <fn1235@bk.ru>
2019-07-01 11:11:12 +0900

    3.5 Стороны прямоугольника

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public int square(int p, int k) {
+         int h = p/(2*(k+1));
+         int w = h*k;
+         int s = w*h;
+        return s;
+    }
+}
