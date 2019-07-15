753c2b0f9c643a2fda44325000118bfb2c8e6f36
Vlad1235 <fn1235@bk.ru>
2019-07-15 12:50:40 +0900

    6.7 исправленный

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -14,12 +14,12 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
 
-            for (int index = 1; index <= data.length - 1; index++) {
+            for (int index = 1; index <= data.length-1; index++) {
                 if (data[0][0] != data[index][index]) {
                     result = false;
                     break;
                 }
-                if (data[0][data.length - 1] != data[index][index]) {
+                if (data[0][data.length-1] != data[index][data.length-1-index]) {
                         result = false;
                         break;
                 }
