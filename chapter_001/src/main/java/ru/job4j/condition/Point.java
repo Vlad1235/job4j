02ba57b53168bf9e9dbb14ccfc5f1d737d31fe86
Vlad1235 <fn1235@bk.ru>
2019-07-22 02:56:17 +0900

    task_150672 Расстояние между точками в трехмерном пространстве

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -9,14 +9,17 @@ import static java.lang.Math.pow;
  */
 public class Point {
     /**
-     * Это поле объекта. Оно доступно только конкретному объекту.
+     * Это переменная экземпляра. Она доступна только конкретному объекту.
      */
     private int x;
-
     /**
-     * И это поле объекта. Оно доступно только конкретному объекту.
+     * Это переменная экземпляра. Она доступна только конкретному объекту.
      */
     private int y;
+    /**
+     * Это переменная экземпляра. Она доступна только конкретному объекту.
+     */
+    private int z;
     /**
      * Конструктор, который принимает начальное состояние объекта "точка"
      * @param first координата x
@@ -26,13 +29,44 @@ public class Point {
         this.x = first;
         this.y = second;
     }
+
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     * @param x
+     * @param y
+     * @param z
+     */
+    public Point(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Конструктор по умолчанию.
+     */
     public Point(){
     }
+    /**
+     * Метод расчета расстояния для двумерного пространства.
+     * @param that
+     * @return
+     */
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
-
+    /**
+     * Метод расчета расстояния для трехмерного пространства.
+     * @param that
+     * @return
+     */
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
     public void info() { // Метод выводит значение переменных экземпляра Х и У
         System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+    public void info3d() { // Метод выводит значение переменных экземпляра Х  У Z
+        System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -17,6 +17,18 @@ public class PointTest {
     }
 
     @Test
+    public void when3dZeroAndTenThenTen() {
+        Point x = new Point(0, 0,0);
+        Point y = new Point(0, 0,10);
+        double result = x.distance3d(y);
+        x.info3d();
+        y.info3d();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(10D));
+    }
+
+
+    @Test
     public void whenCheckItself() {
         Point point = new Point(0, 0);
         double result = point.distance(point);
@@ -29,5 +41,12 @@ public class PointTest {
         first.info();
         Point second = new Point(2, 2);
         second.info();
+    }
+    @Test
+    public void whenShowInfo3d() {
+        Point x = new Point(1, 1,2);
+        x.info3d();
+        Point y = new Point(2, 2,4);
+        y.info3d();
     }
 }
