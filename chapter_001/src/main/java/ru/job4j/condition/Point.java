5160cf607d317c35dc3cc256152e5a578afe663f
Vlad1235 <fn1235@bk.ru>
2019-07-21 22:41:53 +0900

    Task_150670 Рефакторинг - Расстояние между точками

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,4 +1,7 @@
 package ru.job4j.condition;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 /**
  * @author Vladislav (fn1235@bk.ru)
  * @version $Id$
@@ -6,16 +9,30 @@ package ru.job4j.condition;
  */
 public class Point {
     /**
-     * @param x1
-     * @param y1
-     * @param x2
-     * @param y2
-     * @return distance
+     * Это поле объекта. Оно доступно только конкретному объекту.
      */
-    public double distance(int x1, int y1, int x2, int y2) {
-        double first = Math.pow(x2 - x1, 2);
-        double second = Math.pow(y2 - y1, 2);
-        double distance = Math.sqrt(first + second);
-        return distance;
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+    public Point(){
+    }
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public void info() { // Метод выводит значение переменных экземпляра Х и У
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
