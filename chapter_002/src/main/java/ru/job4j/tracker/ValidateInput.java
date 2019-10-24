1b52dfcb6a28a36c7a9794a63f88a904f197b14e
Vlad1235 <fn1235@bk.ru>
2019-10-24 10:56:21 +0900

    task_150662

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -11,38 +11,62 @@ package ru.job4j.tracker;
 
  */
 public class ValidateInput extends ConsoleInput {
+//    @Override
+//    public int askInt(String question) {
+//        boolean invalid = true;
+//        int value = -1;
+//        do {
+//            try {
+//                value = super.askInt(question);
+//                invalid = false;
+//            } catch (NumberFormatException nfe) {
+//                System.out.println("Please enter validate data again ");
+//            }
+//        } while (invalid);
+//        return value;
+//    }
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
-        do {
-            try {
-                value = super.askInt(question);
-                invalid = false;
-            } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again ");
-            }
-        } while (invalid);
+        if (!invalid){
+            throw new NumberFormatException("Please enter validate data again ");
+        }
+        value = super.askInt(question);
+        invalid = false;
         return value;
     }
 
+
+
     @Override
     public int askInt(String question, int max) {
+//        boolean invalid = true;
+////        int value = -1;
+////        do {
+////            try {
+////                value = super.askInt(question, max);
+////                invalid = false;
+////            } catch (IllegalStateException moe) {
+////                System.out.println("Please select key from menu ");
+////            } catch (NumberFormatException nfe) {
+////                System.out.println("Please enter validate data again ");
+////            }
+////        } while (invalid);
+////        return value;
         boolean invalid = true;
         int value = -1;
-        do {
-            try {
-                value = super.askInt(question, max);
-                invalid = false;
-            } catch (IllegalStateException moe) {
-                System.out.println("Please select key from menu ");
-            } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again ");
-            }
-        } while (invalid);
+        if(!invalid){
+            throw new IllegalStateException("Please select key from menu ");
+            throw new NumberFormatException("Please enter validate data again ");
+        }
+        value = super.askInt(question, max);
+        invalid = false;
         return value;
     }
 }
+
+
 //    public class ValidateInput implements Input{
 //
 //    private final Input input;
