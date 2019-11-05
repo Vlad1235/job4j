b93c4f9082ae656c81da5e4c89e393feb5dcd6bb
Vlad1235 <fn1235@bk.ru>
2019-11-06 03:27:39 +0900

    task150631

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -0,0 +1,30 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+/**
+ * public int[][] toArray (List<Integer> list, int rows) {} - метод toArray должен равномерно разбить лист на количество строк
+ * двумерного массива. В методе toArray должна быть проверка - если количество элементов не кратно
+ * количеству строк - оставшиеся значения в массиве заполнять нулями.
+ *
+ * Например в результате конвертации листа со значениями (1,2,3,4,5,6,7)
+ * с разбиением на 3 строки должен получиться двумерный массив {{1, 2, 3} {4, 5, 6} {7, 0 ,0}}
+ */
+public class ConvertList2Array {
+
+    public int[][] toArray2(List<Integer> list, int rows) {
+        int cells = (list.size() / rows) + ((list.size() % rows == 0)? 0 : 1);
+        int[][] array = new int[rows][cells];
+        for (int values:list) {
+        BLOCK1:
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < cells; c++) {
+                    array[r][c] = values;
+                    break BLOCK1;
+                }
+            }
+        }
+        return array;
+    }
+
+}
